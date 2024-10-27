2079ed58c733ba1fcddb95e635e5add8a03f1ca1
Johnny Squardo <johnny.squardo@gmail.com>
2024-10-27 19:03:28 -0400

    added conform.nvim

---
+++ lua/custom/plugins/luarocks.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'camspiers/luarocks',
+    dependencies = {
+      'rcarriga/nvim-notify', -- Optional dependency
+    },
+    opts = {
+      rocks = { 'fzy' }, -- Specify LuaRocks packages to install
+    },
+  },
+}
