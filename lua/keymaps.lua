3b9ccb8ad67fea2f5345ff7d5801279c7d4c7197
Johnny Squardo <johnny.squardo@gmail.com>
2024-10-30 16:46:07 -0400

    Added Netrw.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,8 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Open and close NetRW
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
--- lua/options.lua
+++ lua/options.lua
@@ -62,4 +62,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Netrw remove header
+vim.g.netrw_banner = 0
+
 -- vim: ts=2 sts=2 sw=2 et
